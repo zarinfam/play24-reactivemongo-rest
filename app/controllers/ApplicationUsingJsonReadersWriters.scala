bb5f21da1c62ebb6f7e246906e766a7c9c04882a
zarinfam.s <zarinfam.s@gmail.com>
2015-08-16 08:52:40 +0430

    create project.

---
+++ app/controllers/ApplicationUsingJsonReadersWriters.scala
@@ -0,0 +1,109 @@
+package controllers
+
+import javax.inject.Inject
+
+import scala.concurrent.Future
+
+import play.api.Logger
+import play.api.mvc.{Action, Controller}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+
+// Reactive Mongo imports
+
+import reactivemongo.api.Cursor
+
+import play.modules.reactivemongo.{// ReactiveMongo Play2 plugin
+MongoController,
+ReactiveMongoApi,
+ReactiveMongoComponents
+}
+
+// BSON-JSON conversions/collection
+
+import play.modules.reactivemongo.json._
+import play.modules.reactivemongo.json.collection._
+
+/*
+ * Example using ReactiveMongo + Play JSON library.
+ *
+ * There are two approaches demonstrated in this controller:
+ * - using JsObjects directly
+ * - using case classes that can be turned into JSON using Reads and Writes.
+ *
+ * This controller uses case classes and their associated Reads/Writes
+ * to read or write JSON structures.
+ *
+ * Instead of using the default Collection implementation (which interacts with
+ * BSON structures + BSONReader/BSONWriter), we use a specialized
+ * implementation that works with JsObject + Reads/Writes.
+ *
+ * Of course, you can still use the default Collection implementation
+ * (BSONCollection.) See ReactiveMongo examples to learn how to use it.
+ */
+class ApplicationUsingJsonReadersWriters @Inject()(
+                                                    val reactiveMongoApi: ReactiveMongoApi) extends Controller
+with MongoController with ReactiveMongoComponents {
+
+  /*
+   * Get a JSONCollection (a Collection implementation that is designed to work
+   * with JsObject, Reads and Writes.)
+   * Note that the `collection` is not a `val`, but a `def`. We do _not_ store
+   * the collection reference to avoid potential problems in development with
+   * Play hot-reloading.
+   */
+  def collection: JSONCollection = db.collection[JSONCollection]("persons")
+
+  // ------------------------------------------ //
+  // Using case classes + JSON Writes and Reads //
+  // ------------------------------------------ //
+
+  import play.api.data.Form
+  import models._
+  import models.JsonFormats._
+
+  def create = Action.async {
+    val user = User(29, "John", "Smith", List(
+      Feed("Slashdot news", "http://slashdot.org/slashdot.rdf")))
+    // insert the user
+    val futureResult = collection.insert(user)
+    // when the insert is performed, send a OK 200 result
+    futureResult.map(_ => Ok)
+  }
+
+  def createFromJson = Action.async(parse.json) { request =>
+    /*
+     * request.body is a JsValue.
+     * There is an implicit Writes that turns this JsValue as a JsObject,
+     * so you can call insert() with this JsValue.
+     * (insert() takes a JsObject as parameter, or anything that can be
+     * turned into a JsObject using a Writes.)
+     */
+    request.body.validate[User].map { user =>
+      // `user` is an instance of the case class `models.User`
+      collection.insert(user).map { lastError =>
+        Logger.debug(s"Successfully inserted with LastError: $lastError")
+        Created
+      }
+    }.getOrElse(Future.successful(BadRequest("invalid json")))
+  }
+
+  def findByName(lastName: String) = Action.async {
+    // let's do our query
+    val cursor: Cursor[User] = collection.
+      // find all people with name `name`
+      find(Json.obj("lastName" -> lastName)).
+      // sort them by creation date
+      sort(Json.obj("created" -> -1)).
+      // perform the query and get a cursor of JsObject
+      cursor[User]
+
+    // gather all the JsObjects in a list
+    val futureUsersList: Future[List[User]] = cursor.collect[List]()
+
+    // everything's ok! Let's reply with the array
+    futureUsersList.map { persons =>
+      Ok(persons.toString)
+    }
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,18 @@
+name := """play24-reactivemongo-rest"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq(
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.11.6.play24",
+  specs2 % Test
+)
+
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+
+// Play provides two styles of routers, one expects its actions to be injected, the
+// other, legacy style, accesses its actions statically.
+routesGenerator := InjectedRoutesGenerator
